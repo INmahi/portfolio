f9cea3f52b50413ddb6ba634472d584f5707e2e5
Mahi787 <ishatnoormahi787@gmail.com>
2021-06-11 09:13:25 +0600

    navbar

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,4 @@
 //preloader
-
 var getPopUp = document.getElementById('popup');
 var getDoneBtn = document.getElementById('done_btn');
 var getWebContent = document.getElementById('web_content');
@@ -7,18 +6,18 @@ var getPloader = document.getElementById('loading');
 function runPreloader(){
     getPloader.style.display="none";
     document.body.style.background="#c7c7c7bd";
-
-    setTimeout(()=>{
-        getPopUp.style.display="block";
-        if(getPopUp.style.display="block"){
-           getWebContent.classList.add("allwrap");
-        }
-       
-    },5000);
+    // setTimeout(()=>{
+    //     getPopUp.style.display="block";
+    //     if(getPopUp.style.display="block"){
+    //        getWebContent.classList.add("allwrap");
+    //     }   
+    // },6000);
 }
-
-//////popup
-
+getWebContent.addEventListener("click",()=>{
+    getPopUp.style.display="none";
+    getWebContent.classList.remove("allwrap")
+})
+////popup close
 var getPopUp = document.getElementById('popup');
 var getDoneBtn = document.getElementById('done_btn');
 getDoneBtn.addEventListener("click",()=>{
@@ -26,14 +25,6 @@ getDoneBtn.addEventListener("click",()=>{
     getWebContent.classList.remove("allwrap")
     // getPloader.style.display="none";
 })
- //automatic show popup
-//  window.onload=function(){
-//     setTimeout(() => {
-//         getPopUp.style.display="block";
 
-//         //set time : 2 seconds
-//     }, 2000);
-
-// }
 
 
